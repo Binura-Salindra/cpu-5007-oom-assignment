4a121797c964fbe1a9d852656ff8706a4263f6ba
binura-salindra <binurasalindra@gmail.com>
2022-08-03 20:43:44 +0530

    create junit test classes and write initial cases for channel and user functionalities

---
+++ src/test/java/uk/bolton/oom/controller/impl/ChannelControllerImplTest.java
@@ -0,0 +1,30 @@
+package uk.bolton.oom.controller.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class ChannelControllerImplTest {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void subscribeUserToChannel() {
+
+    }
+
+    @Test
+    void unSubscribeUserFromChannel() {
+    }
+
+    @Test
+    void shareNewPost() {
+    }
+
+    @Test
+    void signUpChannel() {
+    }
+}
---
+++ src/test/java/uk/bolton/oom/controller/impl/UserControllerImplTest.java
@@ -0,0 +1,20 @@
+package uk.bolton.oom.controller.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class UserControllerImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void signUpUser() {
+    }
+
+    @Test
+    void getAllChannelList() {
+    }
+}
