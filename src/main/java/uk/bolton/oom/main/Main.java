f94c1fad38059a3d5240c49b4e28071f9becebdf
binura-salindra <binurasalindra@gmail.com>
2022-08-03 21:36:02 +0530

    created skeletons of GUI in main pages

---
+++ src/main/java/uk/bolton/oom/main/Main.java
@@ -0,0 +1,11 @@
+package uk.bolton.oom.main;
+
+import uk.bolton.oom.view.HomePage;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HomePage homePage = new HomePage();
+        homePage.setVisible(true);
+    }
+}
