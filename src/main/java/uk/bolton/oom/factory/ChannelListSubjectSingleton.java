a327563df87f8d789c4d571b6a8b4b44e820378d
binura-salindra <binurasalindra@gmail.com>
2022-08-12 17:33:59 +0530

    set comments on singleton class

--- src/main/java/uk/bolton/oom/factory/ChannelListSubjectSingleton.java
+++ src/main/java/uk/bolton/oom/factory/ChannelListSubjectSingleton.java
@@ -2,24 +2,33 @@ package uk.bolton.oom.factory;
 
 import uk.bolton.oom.observer.ChannelListSubject;
 
+/**
+ * This class created for get single object of ChannelListSubject
+ */
 public class ChannelListSubjectSingleton {
 
+    // private field that refers to the object
     private final ChannelListSubject channelListSubject;
-    
+
+    // private static reference of class
     private static ChannelListSubjectSingleton channelListSubjectSingleton;
 
+    // private constructor
     private ChannelListSubjectSingleton() {
         this.channelListSubject = new ChannelListSubject();
     }
 
-    public ChannelListSubject getChannelListSubject() {
-        return channelListSubject;
-    }
-
+    // public static method
+    // write code that allows us to create only one object
+    // access the object as per our need
     public static ChannelListSubjectSingleton getInstance() {
         if (channelListSubjectSingleton == null) {
             channelListSubjectSingleton = new ChannelListSubjectSingleton();
         }
         return channelListSubjectSingleton;
+    }
+
+    public ChannelListSubject getChannelListSubject() {
+        return channelListSubject;
     }
 }
